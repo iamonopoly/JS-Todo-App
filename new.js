46056fae059908fb7d4571e20f7b27d38061f104
monopoly1 <washingtonsam84@gmail.com>
2022-02-15 22:57:10 +0000

    first commit

---
+++ new.js
@@ -0,0 +1,24 @@
+// const {
+//     mongodb
+// } = require('mongodb')
+
+// //let db
+// let connectionString = "mongodb+srv://todoAppUser:sam23456789 @cluster0.bbsfn.mongodb.net/TodoApp?retryWrites=true&w=majority"
+// mongodb.connect(connectionString, { useNewUrlParser: true }, () => {
+//     console.log('client')
+// });
+let { MongoClient } = require("mongodb");
+let uri = "mongodb+srv://todoAppUser:sam23456789@cluster0.bbsfn.mongodb.net/TodoApp?retryWrites=true&w=majority";
+let Estring = "mongodb+srv://todoAppUser:sam123456789@cluster0.bbsfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+let db
+
+MongoClient.connect(Estring, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log("Connection failed for some reason");
+    } else {
+        console.log("Connection established - All well");
+        db = client.db();
+    }
+
+
+});
