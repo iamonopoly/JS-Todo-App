c125b9d2861840b3045d33a26005b04727bd0041
monopoly1 <washingtonsam84@gmail.com>
2022-02-17 13:13:50 +0000

    Changes to the first commit

--- new.js
+++ new.js
@@ -8,8 +8,9 @@
 //     console.log('client')
 // });
 let { MongoClient } = require("mongodb");
-let uri = "mongodb+srv://todoAppUser:sam23456789@cluster0.bbsfn.mongodb.net/TodoApp?retryWrites=true&w=majority";
-let Estring = "mongodb+srv://todoAppUser:sam123456789@cluster0.bbsfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+//let uri = "mongodb+srv://todoAppUser:sam23456789@cluster0.bbsfn.mongodb.net/TodoApp?retryWrites=true&w=majority";
+//let Estring = "mongodb+srv://todoAppUser:sam123456789@cluster0.bbsfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+let Estring = "mongodb://127.0.0.1:27017/TodoApp"
 let db
 
 MongoClient.connect(Estring, { useNewUrlParser: true }, (error, client) => {
